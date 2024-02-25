a90aae3502586c9df50bd4d39a5553cdac438e6a
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-02-25 21:22:32 +0600

    added async operation

--- work.js
+++ work.js
@@ -1,6 +1,14 @@
 import {parentPort} from 'worker_threads'
 
-parentPort.on('message', (message) => {
-    const pi = 3.14
-    parentPort.postMessage(pi)
+parentPort.on('message', async (message) => {
+    await sleepAndPrint(5)
 })
+
+
+const sleepAndPrint = async (end) => {
+    for (let i = 1; i <= end; i++) {
+       await new Promise((resolve) => {
+        setTimeout( () => {console.log(i); resolve()}, 1000)
+       })
+    }
+}
